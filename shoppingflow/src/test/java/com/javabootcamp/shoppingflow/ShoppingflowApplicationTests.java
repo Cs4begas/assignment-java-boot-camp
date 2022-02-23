85cbdef480d70d4cf92fbcb26b6432e8dfcc1d30
nattapan <formulae445@gmail.com>
2022-02-23 22:40:01 +0700

    #3 Create project with Spring Web, H2, JPA, and Lombok

---
+++ shoppingflow/src/test/java/com/javabootcamp/shoppingflow/ShoppingflowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javabootcamp.shoppingflow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShoppingflowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
