a5911e5acfe1e58c1c0087d938daaf099cf57452
nattapan <formulae445@gmail.com>
2022-02-24 16:46:18 +0700

    #19 - Create BasketBusinessLogic   - Create Method for businessLogic Post Basket     1. CreateBasket     2. GetSelectedSize     3. GetCustomer     4. GetProduct     5. CreateBasketToSave - Create 1. BasketRepository 2. CustomerRepository for using in BasketBusinessLogic

---
+++ shoppingflow/src/main/java/com/javabootcamp/shoppingflow/businessLogic/BasketBusinessLogic.java
@@ -0,0 +1,77 @@
+package com.javabootcamp.shoppingflow.businessLogic;
+
+import com.javabootcamp.shoppingflow.exception.NotFoundException;
+import com.javabootcamp.shoppingflow.model.entity.*;
+import com.javabootcamp.shoppingflow.model.entity.request.CreateBasketRequest;
+import com.javabootcamp.shoppingflow.repository.BasketRepository;
+import com.javabootcamp.shoppingflow.repository.CustomerRepository;
+import com.javabootcamp.shoppingflow.repository.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+import java.util.Optional;
+
+@Component
+public class BasketBusinessLogic {
+
+    private BasketRepository basketRepository;
+    private CustomerRepository customerRepository;
+    private ProductRepository productRepository;
+
+    @Autowired
+    public BasketBusinessLogic(BasketRepository basketRepository, CustomerRepository customerRepository, ProductRepository productRepository) {
+        this.basketRepository = basketRepository;
+        this.customerRepository = customerRepository;
+        this.productRepository = productRepository;
+    }
+
+    public Basket CreateBasket(Optional<Integer> customerId, CreateBasketRequest basketRequest) {
+        int customerIdValue = customerId.get();
+        int productId = basketRequest.getProductId();
+
+        Customer customer = GetCustomer(customerIdValue);
+        Product product = GetProduct(productId);
+        List<ProductSize> productSize = product.getProductType().getProductSizes();
+        String selectedSize = GetSelectedSize(basketRequest, productId, productSize);
+
+        Basket basket = CreateBasketToSave(customer, product, selectedSize);
+        basketRepository.save(basket);
+
+        return basket;
+    }
+
+    private String GetSelectedSize(CreateBasketRequest basketRequest, int productId, List<ProductSize> productSizes) {
+        ProductSize productSize = productSizes.stream().filter(x -> {
+            return x.getDescription().equals(basketRequest.getSize());
+        }).findFirst().orElseThrow(() -> new NotFoundException(String.format("Not found input size %s of productId %s", basketRequest.getSize(), productId)));
+        return productSize.getDescription();
+    }
+
+    private Customer GetCustomer(int customerIdValue) {
+        Customer customer = customerRepository.findById(customerIdValue).orElseThrow(() -> new NotFoundException(String.format("Not found customerId %d", customerIdValue)));
+        return customer;
+    }
+
+    private Product GetProduct(int productId) {
+        Product product = productRepository.findById(productId).orElseThrow(() -> new NotFoundException(String.format("Not found productId %s", productId)));
+        return product;
+    }
+
+    private Basket CreateBasketToSave(Customer customer, Product product, String selectedSize) {
+        BasketItem basketItem = new BasketItem();
+        basketItem.setProduct(product);
+        basketItem.setItemName(product.getName());
+        basketItem.setItemDiscount(product.getDiscount());
+        basketItem.setItemSize(selectedSize);
+        basketItem.setItemPrice(product.getFullPrice());
+        basketItem.setItemNetPrice(product.getPrice());
+
+        Basket basket = new Basket();
+        basket.setCustomer(customer);
+        basketItem.setBasket(basket);
+        basket.setBasketItem(basketItem);
+
+        return basket;
+    }
+}
