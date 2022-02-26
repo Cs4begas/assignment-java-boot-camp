65cb31175ef44d85f045184503c24a1e8d316999
nattapan <formulae445@gmail.com>
2022-02-26 13:07:28 +0700

    #66  - Remove folder shoppingflow for clean folder structure

--- shoppingflow/src/main/java/com/javabootcamp/shoppingflow/repository/PaymentTypeRepository.java
+++
@@ -1,7 +0,0 @@
-package com.javabootcamp.shoppingflow.repository;
-
-import com.javabootcamp.shoppingflow.model.entity.PaymentType;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PaymentTypeRepository extends JpaRepository<PaymentType, Integer> {
-}
